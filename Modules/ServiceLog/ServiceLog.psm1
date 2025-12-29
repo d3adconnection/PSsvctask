de85d9294f476a4324cc46be3023e1a2df3e1dd4
d3ad connection <d3ad.connection@gmail.com>
2025-12-29 00:19:06 -0600

    Fix module settings initialization in ExampleModule and ServiceLog to use Import-PowerShellDataFile

--- Modules/ServiceLog/ServiceLog.psm1
+++ Modules/ServiceLog/ServiceLog.psm1
@@ -2,7 +2,7 @@
 
 # Initialize module settings
 if (-not $global:ServiceSettings) { throw "Service settings are not initialized." }
-if (-not $script:Settings) { $script:Settings = (Test-ModuleManifest -Path (Join-Path $PSScriptRoot ((Split-Path $PSScriptRoot -Leaf) + '.psd1'))).PrivateData }
+if (-not $script:Settings) { $script:Settings = (Import-PowerShellDataFile -Path (Join-Path $PSScriptRoot ((Split-Path $PSScriptRoot -Leaf) + '.psd1'))).PrivateData }
 
 # Determine log path based on ServiceHost setting
 $script:LogPath = if ($env:COMPUTERNAME -eq $global:ServiceSettings.ServiceHost) { $script:Settings.LocalLogPath } else { $script:Settings.RemoteLogPath }
