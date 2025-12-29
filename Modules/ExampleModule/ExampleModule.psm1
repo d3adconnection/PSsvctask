33d445f4d4e24adb85de72f29a393835d4af93be
d3ad connection <d3ad.connection@gmail.com>
2025-12-29 00:27:43 -0600

    Remove redundant check for global service settings initialization in ExampleModule

--- Modules/ExampleModule/ExampleModule.psm1
+++ Modules/ExampleModule/ExampleModule.psm1
@@ -1,7 +1,6 @@
 ## EXAMPLE MODULE
 
 # Initialize module settings
-if (-not $global:ServiceSettings) { throw "Service settings are not initialized." }
 if (-not $script:Settings) { $script:Settings = (Import-PowerShellDataFile -Path (Join-Path $PSScriptRoot ((Split-Path $PSScriptRoot -Leaf) + '.psd1'))).PrivateData }
 
 # Import modules
